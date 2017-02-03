1db7593d7b21d4db39e428fae72da7beca96bade
youssefsaafan <ys2915@columbia.edu>
2017-02-03 18:41:39 -0500

    Add files via upload

---
+++ MainInput.m
@@ -0,0 +1,11 @@
+prompt={'Height (in)', 'Wdith (in)','Length (in)','thickness (in)', 'Bolt Count (Enter 1-3)','Laser kerf','Sides (Enter 5 or 6 for a open or closed box, respectively.)'};
+title='Box Maker';
+answer=inputdlg(prompt,title);
+h=str2num(answer{1});
+w=str2num(answer{2});
+l=str2num(answer{3});
+t=str2num(answer{4});
+n=str2num(answer{5});
+LK=str2num(answer{6});
+S=str2num(answer{7});
+
